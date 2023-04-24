ca9bd696936c4caf2a17c034c7bf62a3fcf952e2
damian <qaqa.tanski@users.noreply.github.com>
2023-04-24 17:06:08 +0200

    deleting unused method

--- src/main/java/com/dynatrace/currency/utils/validators/Validator.java
+++ src/main/java/com/dynatrace/currency/utils/validators/Validator.java
@@ -7,15 +7,8 @@ public class Validator {
     private static final Pattern quotationPattern = Pattern
             .compile("^(0*(?:[1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5]))$");
 
-    private static final Pattern bigDecimalPattern = Pattern
-            .compile("^[+-]?(?:0|[1-9]\\d{0,2}(?:,?\\d{3})*)(?:\\.\\d+)?$");
-
     public static boolean isQuotationValid(String string) {
         return quotationPattern.matcher(string).find();
-    }
-
-    public static boolean isBigDecimalValid(String string) {
-        return bigDecimalPattern.matcher(string).find();
     }
 
     public static boolean isCurrencyCodeValid(String string) {
