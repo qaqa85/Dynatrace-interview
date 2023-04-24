a4aec8a652cc483423a042f9788106353472545a
damian <qaqa.tanski@users.noreply.github.com>
2023-04-24 16:50:55 +0200

    Adding BuyAskExchangeControllerTest

--- src/main/java/com/dynatrace/currency/buyAskExchange/dto/MajorDifferenceDto.java
+++ src/main/java/com/dynatrace/currency/buyAskExchange/dto/MajorDifferenceDto.java
@@ -1,9 +1,11 @@
 package com.dynatrace.currency.buyAskExchange.dto;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Builder;
 
 import java.util.Currency;
 
+@Builder
 @JsonPropertyOrder({"currencyCode", "difference"})
 public record MajorDifferenceDto(Currency currencyCode, DifferenceDto difference) {
 }
